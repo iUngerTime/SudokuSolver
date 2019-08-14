180b959fcfb4473ad116937126e31ca046a1be1e
Brenton Unger II <brenton.s.unger@live.com>
2019-08-13 18:02:44 -0700

    Initial Push to GitHub. Includes Working project with poor UI and In-progress versioins that update UI. Current versions are broke.

---
+++ SudokuSolver v2/Grid Objects/GridSquare.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace SudokuSolver_v2.Grid_Objects
+{
+   public class GridSquare : INotifyPropertyChanged
+   {
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      //Notify Function
+      void Notify(string propName)
+      {
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+      }
+
+      //Variables
+      int row;
+      int col;
+
+      //Row Property
+      public int Row
+      {
+         get { return row; }
+         set
+         {
+            row = value;
+            Notify("Row");
+         }
+      }
+
+      //Column Property
+      public int Column
+      {
+         get { return col; }
+         set
+         {
+            col = value;
+            Notify("Column");
+         }
+      }
+
+      //Constructors
+      public GridSquare() : this(0, 0) { }
+      public GridSquare(int row, int col)
+      {
+         this.row = row;
+         this.col = col;
+      }
+   }
+}
---
+++ SudokuSolver v2/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using SudokuSolver_v2.Grid_Objects;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SudokuSolver_v2
+{
+   //Class of grid squares defined in XAML
+   public class GridSquares : ObservableCollection<GridSquare> { }
+
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
+
+      //Row 1
+      GridSquares UpperLeftSquares;
+      GridSquares UpperMiddleSquares;
+      GridSquares UpperRightSquares;
+
+      //Row 2
+      GridSquares MiddleLeftSquares;
+      GridSquares CenterSquares;
+      GridSquares MiddleRightSquares;
+
+      //Row 3
+      GridSquares LowerLeftSquares;
+      GridSquares LowerMiddleSquares;
+      GridSquares LowerRightSquares;
+
+      private void Window_Loaded(object sender, RoutedEventArgs e)
+      {
+         //Observable Collections
+         this.UpperLeftSquares = new GridSquares();
+         this.UpperMiddleSquares = new GridSquares();
+         this.UpperRightSquares = new GridSquares();
+
+         this.MiddleLeftSquares = new GridSquares();
+         this.CenterSquares = new GridSquares();
+         this.MiddleRightSquares = new GridSquares();
+
+         this.LowerLeftSquares = new GridSquares();
+         this.LowerMiddleSquares = new GridSquares();
+         this.LowerRightSquares = new GridSquares();
+
+         //SET DATA CONTEXTS
+         //TO DO HERE
+
+         //butt.Style = this.FindResource("SelectedToggleButton") as Style;
+         //butt.Content = 1;
+      }
+   }
+}
