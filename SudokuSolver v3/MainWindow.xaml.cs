867aae7741713e7425c5dad30596aa859eacb7a7
Brenton Unger II <brenton.s.unger@live.com>
2019-11-17 12:30:36 -0800

    Created a new working Version. Is able to run but lacks most functionality. Purpose for V3 is a working version but no XAML collection dynamic controls.

---
+++ SudokuSolver v3/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SudokuSolver_v3
+{
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      //CurrentSelected Number
+      int m_selectedValue;
+
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
+
+      private void RadioButton_Click(object sender, RoutedEventArgs e)
+      {
+         if (Convert.ToString((sender as ToggleButton).Content) == "X")
+            m_selectedValue = 0;
+         else
+            m_selectedValue = Convert.ToInt32((sender as ToggleButton).Content);
+      }
+   }
+}
